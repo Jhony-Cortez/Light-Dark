f65827ec7f2709c93cb14a84dc06379e991504f4
Jhony-Cortez <jhony.cortez1@gmail.com>
2022-08-26 08:32:27 -0300

    update

---
+++ script.js
@@ -0,0 +1,7 @@
+function mudar(tipo) {
+    if (tipo == 1) {
+        document.getElementById('on').href ='style2.css'
+    } else if (tipo == 2 ) {
+        document.getElementById('on').href ='style.css'
+    }
+} 
